d04808039cbf24b624ddae4a3337cb3574e11398
Erlangga15 <angga.hendrika@gmail.com>
2024-09-03 15:57:30 +0700

    feat: add exercise file

---
+++ exercise/src/main/java/com/erlangga/exercise/InitalName.java
@@ -0,0 +1,23 @@
+package com.erlangga.exercise;
+
+import java.util.Scanner;
+
+public class InitalName {
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Enter your name: ");
+			String name = scanner.nextLine();
+			getInitialName(name);
+		}
+	}
+
+	public static void getInitialName(String name) {
+		String[] nameParts = name.split(" ");
+		if (nameParts.length >= 2) {
+			String initialName = String.valueOf(nameParts[0].charAt(0)) + String.valueOf(nameParts[1].charAt(0));
+			System.out.println("The initial name is: " + initialName);
+		} else {
+			System.out.println("Please enter at least two names.");
+		}
+	}
+}
---
+++ exercise/src/main/java/com/erlangga/exercise/Triangle.java
@@ -0,0 +1,20 @@
+package com.erlangga.exercise;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Masukkan sudut pertama segitiga: ");
+            int angleOne = scanner.nextInt();
+            System.out.print("Masukkan sudut kedua segitiga: ");
+            int angleTwo = scanner.nextInt();
+            int angleThree = calculateAngleThree(angleOne, angleTwo);
+            System.out.printf("Sudut ketiga segitiga adalah: %d\n", angleThree);
+        }
+    }
+
+    public static int calculateAngleThree(int angleOne, int angleTwo) {
+        return 180 - angleOne - angleTwo;
+    }
+}
