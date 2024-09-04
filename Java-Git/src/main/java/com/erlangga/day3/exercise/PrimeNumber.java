f3f567c47057ef8e95522a6ee3168aad3eaef540
Erlangga15 <angga.hendrika@gmail.com>
2024-09-04 15:13:45 +0700

    fix: fix bug PrimeNumber

--- Java-Git/src/main/java/com/erlangga/day3/exercise/PrimeNumber.java
+++ Java-Git/src/main/java/com/erlangga/day3/exercise/PrimeNumber.java
@@ -8,6 +8,20 @@ public class PrimeNumber {
 	}
 
 	public boolean isPrime() {
-		return number > 1 && number % 2 != 0;
+		if (number <= 1) {
+			return false;
+		}
+		if (number <= 3) {
+			return true;
+		}
+		if (number % 2 == 0 || number % 3 == 0) {
+			return false;
+		}
+		for (int i = 5; i * i <= number; i += 6) {
+			if (number % i == 0 || number % (i + 2) == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
